14f76a8e233a3f5ac1a25826ee84d78ece41ccb2
QunCAO1 <clothcao@gmail.com>
2022-12-10 00:08:12 +0800

    test

---
+++ webProject/src/components/article.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="">
+    <header class="w3-container w3-light-gray">
+      <p class="w3-btn w3-round-xlarge w3-padding-large"><router-link :to="{ name: 'salesinfo', params: {salesID: salesID} }"><img :src="picIMG" width="30" height="30" class="w3-margin-right">{{salesID}}</router-link></p>
+    </header>
+    <div>
+      <button class="w3-btn w3-tiny w3-right w3-black" v-on:click="edit=!edit">Edit</button>
+    </div>
+
+    <div class=" w3-container">
+      <ul>
+        <li v-if="hvArt" v-for="art in artList" class="w3-bar w3-border-bottom">
+          <span class="w3-bar-item"><i v-if="art.status == 1" class="material-icons">language</i><i v-if="art.status == 0" class="material-icons">mode_edit</i></span>
+          <span v-if="edit==true && art.status == 0" class="w3-btn w3-bar-item w3-right w3-text-red"><i class="material-icons">remove_circle</i></span>
+          <div class="w3-bar-item">
+            <span class="w3-large">{{art.title}}</span>
+          </div>
+        </li>
+      </ul>
+    </div>
+    <div v-if="!hvArt" class="w3-display-middle w3-text-gray w3-xlarge">
+      <p>You don't have any article!</p>
+    </div>
+    <div class="w3-text-blue-gray w3-display-bottomright">
+      <router-link :to="{ name: 'createArticle', params: {salesID: salesID} }"><i class="w3-btn w3-circle w3-ripple medium material-icons">add_circle</i></router-link>
+    </div>
+
+
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+import router from '../router'
+
+export default {
+  data () {
+    return {
+      edit: false,
+      artList: [],
+      errorCode: "",
+      salesID: "",
+      hvArt: false,
+      picIMG: ""
+    }
+  },
+
+    methods:{
+
+    },
+
+    created() {
+      this.salesID = this.$route.params.salesID;
+      console.log(this.$route.params.salesID);
+      axios.post('http://localhost:3000/getArticleList',{
+        salesID: this.salesID
+        })
+      .then(response => {
+          console.log(response);
+          this.artList = response.data.rows;
+          this.errorCode = response.data.code;
+          console.log(this.errorCode);
+
+          if(this.errorCode == 121)
+          {
+            console.log("go into 121");
+            this.hvArt = false;
+          }
+          else
+            this.hvArt = true;
+
+            axios.post('http://localhost:3000/getSalesIMG', {
+              salesID: this.salesID
+            }).then(response => {
+              console.log(response);
+              this.picIMG = response.data.rows[0].profileIMG;
+            })
+      })
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ webProject/src/router/index.js
@@ -0,0 +1,202 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Mint from 'mint-ui'
+import 'mint-ui/lib/style.css'
+import VueCal from 'vue-cal'
+import 'vue-cal/dist/vuecal.css'
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import 'materialize-css'
+import 'materialize-css/dist/css/materialize.css'
+import 'w3-css/w3.css'
+import 'w3-css/4/w3pro.css'
+import 'w3-css/4/w3mobile.css'
+import VeeValidate from 'vee-validate';
+import HelloWorld from '@/components/HelloWorld'
+import login from '@/components/login'
+import register from '@/components/register'
+import events from '@/components/events'
+import eventInfo from '@/components/eventInfo'
+import registerEvent from '@/components/registerEvent'
+import companyLogin from '@/components/companyLogin'
+import companyHome from '@/components/companyHome'
+import comEventRes from '@/components/comEventRes'
+import comEventInfo from '@/components/comEventInfo'
+import salesLogin from '@/components/salesLogin'
+import salesRegister from '@/components/salesRegister'
+import article from '@/components/article'
+import userinfo from '@/components/userinfo'
+import editinfo from '@/components/editinfo'
+import changepw from '@/components/changepw'
+import companypw from '@/components/companypw'
+import editcominfo from '@/components/editcominfo'
+import salesedit from '@/components/salesedit'
+import salesinfo from '@/components/salesinfo'
+import salespw from '@/components/salespw'
+import companyinfo from '@/components/companyinfo'
+import createArticle from '@/components/createArticle'
+import salesInfoPage from '@/components/salesInfoPage'
+import test from '@/components/test'
+import readArticle from '@/components/readArticle'
+
+Vue.use(BootstrapVue);
+Vue.use(Router)
+Vue.use(Mint)
+Vue.use(VeeValidate);
+
+export default new Router({
+  routes: [
+    {
+      path:'./readArticle',
+      name:'readArticle',
+      component: readArticle
+    },
+    {
+      path: '/',
+      name: 'login',
+      component: login
+    },
+    {
+      path:'/test',
+      name:'test',
+      component: test
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: register
+    },
+
+    {
+      path: '/events/:userID',
+      name: 'events',
+      component: events
+    },
+
+    {
+      path: '/eventInfo/:userID/:eventID',
+      name: 'eventInfo',
+      component: eventInfo
+    },
+
+    {
+      path: '/registerEvent/:userID/:eventID',
+      name: 'registerEvent',
+      component: registerEvent
+    },
+
+    {
+      path: '/companyLogin',
+      name: 'companyLogin',
+      component: companyLogin
+    },
+
+    {
+      path: '/companyHome/:companyID',
+      name: 'companyHome',
+      component: companyHome
+    },
+
+    {
+      path: '/comEventRes/:companyID',
+      name: 'comEventRes',
+      component: comEventRes
+    },
+
+    {
+      path: '/comEventInfo/:companyID/:eventID',
+      name: 'comEventInfo',
+      component: comEventInfo
+    },
+
+    {
+      path: '/salesLogin',
+      name: 'salesLogin',
+      component: salesLogin
+    },
+
+    {
+      path: '/salesRegister',
+      name: 'salesRegister',
+      component: salesRegister
+    },
+
+    {
+      path: '/article/:salesID',
+      name: 'article',
+      component: article
+    },
+
+    {
+      path: '/userinfo/:userID',
+      name: 'userinfo',
+      component: userinfo
+    },
+
+    {
+      path: '/editinfo/:userID',
+      name: 'editinfo',
+      component: editinfo
+    },
+
+    {
+      path: '/changepw/:userID',
+      name: 'changepw',
+      component: changepw
+    },
+
+    {
+      path: '/companypw/:companyID',
+      name: 'companypw',
+      component: companypw
+    },
+
+    {
+      path: '/editcominfo/:companyID',
+      name: 'editcominfo',
+      component: editcominfo
+    },
+
+    {
+      path: '/salesedit/:salesID',
+      name: 'salesedit',
+      component: salesedit
+    },
+
+    {
+      path: '/salesinfo/:salesID',
+      name: 'salesinfo',
+      component: salesinfo
+    },
+
+    {
+      path: '/salespw/:salesID',
+      name: 'salespw',
+      component: salespw
+    },
+
+    {
+      path: '/companyinfo/:companyID',
+      name: 'companyinfo',
+      component: companyinfo
+    },
+
+    {
+      path: '/createArticle/:salesID',
+      name: 'createArticle',
+      component: createArticle
+    },
+    /*
+    {
+      path: '/createArticle',
+      name: 'createArticle',
+      component: createArticle
+    },*/
+    {
+      path: '/salesInfoPage/:salesID',
+      name: 'salesInfoPage',
+      component: salesInfoPage
+    }
+  ]
+})
